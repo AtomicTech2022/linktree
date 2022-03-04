557290fa674b5263b75fef0a136aa576c534682f
AtomicTech_kyzel <100929446+AtomicTech2022@users.noreply.github.com>
2022-03-04 19:25:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Hacker", "Developer", "Blogger", "Designer", "IT", "Founder of AtomicTech"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
